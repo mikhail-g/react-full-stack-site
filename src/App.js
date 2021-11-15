5d21b2a9c1767d41507bd570b96dc79e26035e85
Mykhailo Hariachyi <m.gariachiy@gmail.com>
2021-11-15 04:56:20 -0500

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ArticlesList from './pages/ArticlesList'
 import ArticlePage from './pages/ArticlePage'
+import NavBar from './components/NavBar'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className='App'>
+        <NavBar />
         <div id='page-body'>
           <Routes>
             <Route path='/' element={<HomePage />} />
