ae47ae240cb77f96d8588f4fc08ebd86df2d7d15
Mykhailo Hariachyi <m.gariachiy@gmail.com>
2021-11-15 06:00:50 -0500

    Aded Other Articles to the Article page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
 import articleContent from './article-content'
+import ArticlesList from '../components/ArticlesList'
 
 const ArticlePage = () => {
   const {name} = useParams()
   const article = articleContent.find(art => art.name === name)
- 
-    if (!article) return <h1>Article does not exist!</h1>
 
+  if (!article) return <h1>Article does not exist!</h1>
+
+  const otherArticles = articleContent.filter(art => art.name !== name)
   return (
     <>
       <h1>{article.title}</h1>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
     </>
   )
-}
+} 
 
 export default ArticlePage
