a75cdb4312e4ee64f4c1bfa6e893b8b457ce3998
Mykhailo Hariachyi <m.gariachiy@gmail.com>
2021-11-15 05:15:29 -0500

    Added Articles content and routing

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
+import articleContent from './article-content'
 
 const ArticlePage = () => {
+  const {name} = useParams()
+  const article = articleContent.find(art => art.name === name)
+ 
+    if (!article) return <h1>Article does not exist!</h1>
 
   return (
     <>
-      <h1>This is an article</h1>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
     </>
   )
 }
