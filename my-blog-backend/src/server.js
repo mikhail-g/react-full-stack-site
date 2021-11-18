f8ffb275835b14b8e05659667384f99dd1f9991c
Mykhailo Hariachyi <m.gariachiy@gmail.com>
2021-11-18 04:39:52 -0500

    Added connection to mongodb Refactored upvotes

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,37 +1,57 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-
-const articlesInfo = {
-  'learn-react': {
-    upvotes: 0,
-    comments: []
-  },
-  'learn-node': {
-    upvotes: 0,
-    comments: []
-  },
-  'my-thoughts-on-resumes': {
-    upvotes: 0,
-    comments: []
-  }
-}
+import {MongoClient} from 'mongodb'
 
 const app = express()
 
-app.use(bodyParser.json())
+app.use(express.json())
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-  const articleName = req.params.name
+app.get('/api/articles/:name', async (req, res) => {
+  try {
+    const articleName = req.params.name
 
-  articlesInfo[articleName].upvotes += 1
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`)
+    const client = await MongoClient.connect('mongodb://127.0.0.1:27017', {useNewUrlParser: true})
+    const db = client.db('my-blog')
+
+    const articleInfo = await db.collection('articles').findOne({name: articleName})
+    res.status(200).json(articleInfo)
+
+    client.close()
+  } catch (error) {
+    res.status(500).json({message: 'Error connecting to db', error})
+  }
+})
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+  try {
+    const articleName = req.params.name
+
+    const client = await MongoClient.connect('mongodb://127.0.0.1:27017', {useNewUrlParser: true})
+    const db = client.db('my-blog')
+
+    const articleInfo = await db.collection('articles').findOne({name: articleName})
+    await db.collection('articles').updateOne(
+      {name: articleName},
+      {
+        $set: {
+          upvotes: articleInfo.upvotes + 1
+        }
+      }
+    )
+    const updatedArticleInfo = await db.collection('articles').findOne({name: articleName})
+    res.status(200).json(updatedArticleInfo)
+
+    client.close()
+  } catch (error) {
+    res.status(500).json({message: 'Error connecting to db', error})
+  }
 })
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
-  const articleName = req.params.name
   const {username, text} = req.body
+  const articleName = req.params.name
 
   articlesInfo[articleName].comments.push({username, text})
+
   res.status(200).send(articlesInfo[articleName])
 })
 
