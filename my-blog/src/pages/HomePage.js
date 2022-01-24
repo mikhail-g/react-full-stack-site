ca0c27fc9161d72eccdfaebcd0f31b0016a6c3db
Mykhailo Hariachyi <m.gariachiy@gmail.com>
2022-01-24 13:03:49 -0500

    test1 commit

--- my-blog/src/pages/HomePage.js
+++ my-blog/src/pages/HomePage.js
@@ -21,7 +21,7 @@ const HomePage = () => (
       interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec
       finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum,
       nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui
-      in facilisis aliquet.,
+      in facilisis aliquet. ,
     </p>
   </>
 )
