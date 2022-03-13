093bbd0d6cfa9e4a53807fe4cea3534568236fef
Mykhailo Hariachyi <m.gariachiy@gmail.com>
2022-03-13 17:08:19 -0400

    Added Upvotes * added proxy for local dev server * added fetch polifill for IE

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import articleContent from './article-content'
 import NotFoundPage from './NotFoundPage'
@@ -8,12 +8,25 @@ const ArticlePage = () => {
   const {name} = useParams()
   const article = articleContent.find(art => art.name === name)
 
+  const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`)
+      const body = await result.json()
+      console.log(body)
+      setArticleInfo(body)
+    }
+    fetchData()
+  }, [name])
+
   if (!article) return <NotFoundPage />
 
   const otherArticles = articleContent.filter(art => art.name !== name)
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
